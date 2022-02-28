6b712abfa1e446e9dd00ebdbc6c04eba00be2be3
Christine Gogo <christine.gogo@pnmac.com>
2022-02-28 09:58:51 -0500

    fixed syntax 14_longest_common_prefix.js

--- 14_longest_common_prefix.js
+++ 14_longest_common_prefix.js
@@ -7,15 +7,13 @@ If there is no common prefix, return an empty string "". */
  * @return {string}
  */
  var longestCommonPrefix = function(strs) {
-    prefix = strs[0];
+    let prefix = strs[0];
     for (let i = 1; i < strs.length; i++) {
         while(strs[i].indexOf(prefix) != 0) {
             prefix = prefix.substring(0, prefix.length - 1);
         }        
     }
-
     return prefix
-    
 };
 
 // Test one
@@ -23,9 +21,3 @@ console.log(longestCommonPrefix(["flower", "flow", "flight"]));
 
 // Test two
 console.log(longestCommonPrefix(["dog","racecar","car"]));
-
-
-// prefix = "flowe"
-// prefix = prefix.substring(0, prefix.length - 1)
-// console.log(prefix)
-// console.log("flow".indexOf(prefix));
