24a7d31c4342994bc013716f76c9dac2b3e3d692
ChristineGoGo <brandywang22@gmail.com>
2022-07-26 06:32:53 -0400

    white space 1394_find_lucky_integer_in_an_array.js

--- 1394_find_lucky_integer_in_an_array.js
+++ 1394_find_lucky_integer_in_an_array.js
@@ -7,19 +7,19 @@
  var findLucky = function(arr) {
     let largest = 0;
     let m = new Map();
-    
+
     for (let i = 0; i < arr.length; i++) {
         let curr = m.get(arr[i]);
         if (curr === undefined){
-            m.set(arr[i], 1)   
+            m.set(arr[i], 1)
         } else {
             m.set(arr[i], curr += 1 );
         }
     }
-    
+
     for ([key, value] of m) {
         if (key === value) largest = Math.max(largest, key);
     }
-    
+
     return largest === 0 ? -1 : largest;
 };
