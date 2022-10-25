69057f438cb718ad75682eb25abc711fa8e14ca7
ChristineGoGo <brandywang22@gmail.com>
2022-10-25 16:47:39 -0400

    5_Longest_Palindromic_Substring.py

--- 5_Longest_Palindromic_Substring.py
+++ 5_Longest_Palindromic_Substring.py
@@ -1,28 +1,93 @@
-# Brute Force
-def longest_substring(s):
-    def isPalindrome(s):
-        start, end = 0, len(s) - 1
-        while start < end:
-            while start < end and not s[start].isalnum():
-                start += 1
-            while start < end and not s[end].isalnum():
-                end -= 1
-            if s[start].lower() != s[end].lower():
-                return False
+#-------------  brute force - time limit exceeded
+# def longestPalindrome(s):
+#     n = len(s)
+#     res = ""
 
-            start += 1
-            end -= 1
-        return True
-
-    common_subs = {}
-    for i in range(0, len(s)):
-        for j in range(1, len(s)+1):
-            if isPalindrome(s[i:j]):
-                common_subs[s[i:j]] = len(s[i:j])
-    print(common_subs)
-    return max(common_subs, key=common_subs.get)
+#     for i in range(n):
+#         for j in range(i, n + 1):
+#             if self.isPalindrome(s[i:j]):
+#                 if len(res) < (j - i):
+#                     res = s[i:j]
 
 
-if __name__ == '__main__':
-    sample_string = 'aaabbaa'
-    print(longest_substring(sample_string))
+#     return res
+
+def isPalindrome(s):
+    n_s = ""
+    for i in range(len(s) -  1, -1, -1):
+        n_s += s[i]
+    return n_s == s
+#------------------- two pointers
+# class Solution:
+#     res = ""
+#     def longestPalindrome(self, s):
+#         n = len(s)
+#         def checkPalindrome(b, e):
+#             while ((b >= 0 and e < n) and (s[b] == s[e])):
+#                 curr_l = (e - b) + 1
+#                 if curr_l > len(self.res):
+#                     self.res = s[b: e + 1]
+#                 b -= 1
+#                 e += 1
+#         for i in range(n):
+#             checkPalindrome(i, i)
+#             checkPalindrome(i, i + 1)
+
+#         return self.res
+
+#------------------- dynamic programming
+class Solution:
+    res = ""
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        # set up the dynamic programming table
+        dp = [[0] * (n) for i in range(n)]
+        for i in range(n):
+            dp[i][i] = True
+
+        # populate the rest of the table starting w
+        # last index
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n, 1):
+                # two conditions of a palindromic substring:
+                #   - outer characters are equal and inner word is a
+                #     palindrome
+                # inner word is a palindrome if dp[i + 1][j - 1]
+                # is True or if it has one character
+                if s[i] == s[j]:
+                    if (j - i) == 1 or j == i or dp[i + 1][j -1]:
+                        dp[i][j] = True
+                        if len(self.res) < (j - i) + 1:
+                            self.res = s[i:j +1]
+                else:
+                    dp[i][j] = False
+
+        return self.res
+
+
+# s = """
+#     civilwartestingwhetherthatnaptionoranynartionsoconceivedand
+#     sodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwar
+#     Wehavecometodedicpateaportionofthatfieldasafinalrestingplacefor
+#     thosewhoheregavetheirlivesthatthatnationmightliveItisaltogethe
+#     rfangandproperthatweshoulddothisButinalargersensewecannotdedica
+#     tewecannotconsecratewecannothallowthisgroundThebravelmenlivingand
+#     deadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddor
+#     detractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebut
+#     itcanneverforgetwhattheydidhereItisforusthelivingrathertobededicated
+#     heretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadva
+#     ncedItisratherforustobeherededicatedtothegreattdafskremainingbeforeu
+#     sthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhich
+#     theygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthes
+#     edeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirth
+#     offreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth
+# """
+# print(f"longestPalindrome(s): {longestPalindrome(s)} ") #ranynar
+
+s = "babad"
+test = Solution()
+print(f"longestPalindrome(s): {test.longestPalindrome(s)} ") #aba
+# s = "abba"
+# test = Solution()
+# print(f"longestPalindrome(s): {test.longestPalindrome(s)} ") #abba
+
