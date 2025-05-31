f28e2df4cbf87bade20b54f832c5516d9cacbc06
ChristineGoGo <brandywang22@gmail.com>
2025-05-31 05:30:52 -0700

    problem 2729

---
+++ math/checkIfTheNumberIsFascinating_2729.java
@@ -0,0 +1,68 @@
+
+import java.util.*;
+
+/*
+ * You are given an integer n that consists of exactly 3 digits.
+ * We call the number n fascinating if, after the following modification, 
+ * the resulting number contains all the digits from 1 to 9 exactly once and does not contain any 0's:
+ * Concatenate n with the numbers 2 * n and 3 * n.
+ * Return true if n is fascinating, or false otherwise.
+ * Concatenating two numbers means joining them together. For example, the concatenation of 121 and 371 is 121371.
+ *  Example 1:
+ *  Input: n = 192
+ *      Output: true
+ *      Explanation: We concatenate the numbers n = 192 and 2 * n = 384 and 3 * n = 576. 
+ *      The resulting number is 192384576. This number contains all the digits from 1 to 9 exactly once.
+ * Example 2:
+ *      Input: n = 100
+ *      Output: false
+ *      Explanation: We concatenate the numbers n = 100 and 2 * n = 200 and 3 * n = 300.
+ *      The resulting number is 100200300. This number does not satisfy any of the conditions.
+ * Constraints:
+ * 100 <= n <= 999 
+ */
+
+ /*
+  * APPROACH : create an array to store all the digits from 1 to 9
+  * perform the concatination of n and n*2 and n* 3. Store the result and transform it into a
+  * hashmap with all the number frequency
+  * return false if the frequency is more than one or number is not found in the all digits 
+  * array
+  */
+public class checkIfTheNumberIsFascinating_2729 {
+    public static boolean isFascinating(int n) {
+        String concatinated = Integer.toString(n) + Integer.toString(n*2) + Integer.toString(n * 3);
+
+        Long formedNumber = Long.parseLong(concatinated);
+        Map<Integer, Integer> numberFrequency = new HashMap<>();
+        
+        int[] allDigits = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+
+        while (formedNumber > 0 ) {
+            int current = (int) (long) (formedNumber % 10);
+            if (numberFrequency.get(current) == null) {
+                numberFrequency.put(current, 1);
+            } else if (numberFrequency.get(current) > 1 || current == 0) {
+                return false;
+            }
+            int frequency = numberFrequency.get(current);
+            frequency++;
+            numberFrequency.put(current, frequency);
+            formedNumber /= 10;
+        }
+
+        for (int i = 0; i < allDigits.length; i++) {
+            if (numberFrequency.get(allDigits[i]) == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        boolean result = isFascinating(192);
+        System.out.println(result);
+    }
+    
+}
