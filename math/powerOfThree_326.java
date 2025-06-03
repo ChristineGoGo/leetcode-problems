8129757b7a9f2306e54773620c678562d7230b92
ChristineGoGo <brandywang22@gmail.com>
2025-06-03 05:45:21 -0700

    leetcode 326

---
+++ math/powerOfThree_326.java
@@ -0,0 +1,23 @@
+public class powerOfThree_326 {
+    public static boolean isPowerOfThree(int n) {
+        int num = n;
+        while (num > 1) {
+            // System.out.println(num);
+            if (num % 3 != 0) {
+                return false;
+            }
+            num /= 3;
+        }
+        return num == 1 ? true : false;
+    }
+
+
+
+    public static void main(String[] args) {
+        int test = 9;
+        System.out.println(isPowerOfThree(test));
+        System.out.println(Math.cbrt(9.0));
+
+    }
+    
+}
