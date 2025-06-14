8708ddada047ab3091b8df3233ea6957030a1243
ChristineGoGo <brandywang22@gmail.com>
2025-06-14 12:45:01 -0700

    leetcode 3289

---
+++ math/theTwoSneakyNumbersofDigitVille_3289.java
@@ -0,0 +1,89 @@
+import java.util.*;
+/*
+In the town of Digitville, there was a list of numbers called nums containing integers from 0 to n - 1. 
+Each number was supposed to appear exactly once in the list, however, two mischievous numbers sneaked 
+in an additional time, making the list longer than usual.
+
+As the town detective, your task is to find these two sneaky numbers. Return an array of size two 
+containing the two numbers (in any order), so peace can return to Digitville.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,1,0]
+
+Output: [0,1]
+
+Explanation:
+
+The numbers 0 and 1 each appear twice in the array.
+
+Example 2:
+
+Input: nums = [0,3,2,1,3,2]
+
+Output: [2,3]
+
+Explanation:
+
+The numbers 2 and 3 each appear twice in the array.
+
+Example 3:
+
+Input: nums = [7,1,5,4,3,4,6,0,9,5,8,2]
+
+Output: [4,5]
+
+Explanation:
+
+The numbers 4 and 5 each appear twice in the array.
+
+ 
+
+Constraints:
+
+2 <= n <= 100
+nums.length == n + 2
+0 <= nums[i] < n
+The input is generated such that nums contains exactly two repeated elements.
+ 
+*/
+
+/*
+ * APPROACH: Use a hashmap to track the frequency of each number
+ * store the numbers with a frequency of more than one in the results
+ * array. return the result.
+ */
+
+
+public class theTwoSneakyNumbersofDigitVille_3289 {
+    public static int[] getSneakyNumbers(int[] nums) {
+        Map<Integer, Integer> m = new HashMap<>();
+        int n = nums.length;
+        int[] result = new int[2];
+        int position = 0;
+        int current;
+
+        for(int i =0; i < n; i++) {
+            if (m.get(nums[i]) == null) {
+                m.put(nums[i], 1);
+            } else {
+                current = m.get(nums[i]);
+                current++;
+                m.put(nums[i], current);
+            }
+            if (m.get(nums[i]) > 1) {
+                result[position] = nums[i];
+                position++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,3,2,1,3,2};
+        int[] result = getSneakyNumbers(nums);
+        System.out.println(Arrays.toString(result));
+    }
+}
