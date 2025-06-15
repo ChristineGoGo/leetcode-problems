9f8131f5ce306ac95f509af4b884e4060b5cec5f
ChristineGoGo <brandywang22@gmail.com>
2025-06-14 17:59:18 -0700

    leetcode 2965

---
+++ math/findMissingAndRepeatedValue_2965.java
@@ -0,0 +1,97 @@
+
+import java.util.*;
+/*
+
+ You are given a 0-indexed 2D integer matrix grid of size n * n with values in the range
+  [1, n2]. Each integer appears exactly once except a which appears twice and b which is missing. 
+ The task is to find the repeating and missing numbers a and b.
+
+ Return a 0-indexed integer array ans of size 2 where ans[0] equals to a and ans[1] equals to b.
+ Example 1:
+
+Input: grid = [[1,3],[2,2]]
+Output: [2,4]
+Explanation: Number 2 is repeated and number 4 is missing so the answer is [2,4].
+Example 2:
+
+Input: grid = [[9,1,7],[8,9,2],[3,4,6]]
+Output: [9,5]
+Explanation: Number 9 is repeated and number 5 is missing so the answer is [9,5].
+ 
+
+Constraints:
+
+2 <= n == grid.length == grid[i].length <= 50
+1 <= grid[i][j] <= n * n
+For all x that 1 <= x <= n * n there is exactly one x that is not equal to any of the grid members.
+For all x that 1 <= x <= n * n there is exactly one x that is equal to exactly two of the grid members.
+For all x that 1 <= x <= n * n except two of them there is exactly one pair of i, j that 0 <= i, j <= n - 1 and grid[i][j] == x.
+ */
+
+/*
+ * APPROACH: create an array that stores the repeated and missing number.
+ * create a variable to store the maximum range number, which is the length
+ * of the given matrix squared.
+ * Loop through each number in the matrix and add each number to an array, sort the array. 
+ * Loop through the array to find the missing number and duplicated number
+ */
+public class findMissingAndRepeatedValue_2965 {
+    public static int[] findMissingAndRepeatedValues(int[][] grid) {
+        int[] result = new int[2];
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        int n = grid.length;
+        // int range = n * n;
+        int position = 1;
+        int current;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (frequencyMap.get(grid[i][j]) == null) {
+                    frequencyMap.put(grid[i][j], 1);
+                } else {
+                    current = frequencyMap.get(grid[i][j]);
+                    current ++;
+                    frequencyMap.put(grid[i][j], current);
+                }
+            }
+        }
+
+        List<Integer> foundNumbers = new ArrayList<>(frequencyMap.keySet());
+        Collections.sort(foundNumbers);
+        // System.out.println(foundNumbers);
+
+        for (int i = 0; i < foundNumbers.size(); i++) {
+            int num = foundNumbers.get(i);
+            int c = frequencyMap.get(num);
+
+            // System.out.println(c);
+            // System.out.println(position);
+
+            if (num != position) {
+                result[1] = position;
+                position = num;
+            } 
+            
+            if (c == 2) {
+                result[0] = num;
+            }
+            position++;
+        }
+        if (result[1] == 0) {
+            result[1] = position;
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        int[][] m = {{1,3},{2,2}};
+        int[][] m1 = {{9,1,7},{8,9,2},{3,4,6}};
+        int[] result = findMissingAndRepeatedValues(m);
+        int[] result1 = findMissingAndRepeatedValues(m1);
+        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(result1));
+    }
+    
+}
