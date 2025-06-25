632ee272bf2da3a776d6082e6ac76334478273eb
ChristineGoGo <brandywang22@gmail.com>
2025-06-25 06:51:31 -0700

    leetcode 1979

--- math/greatestCommonDivisorOfArray_1979.java
+++ math/greatestCommonDivisorOfArray_1979.java
@@ -1,5 +1,5 @@
 
-import java.util.Arrays;
+import java.util.*;
 /*
  * Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
  * The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
