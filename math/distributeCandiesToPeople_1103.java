1e9b2e3a70831ecce7d11587f4df3be2c05dec57
ChristineGoGo <brandywang22@gmail.com>
2025-07-04 18:10:59 -0700

    leetcode 1103

--- math/distributeCandiesToPeople_1103.java
+++ math/distributeCandiesToPeople_1103.java
@@ -1,11 +1,15 @@
+import java.util.*;
+
 /*
     We distribute some number of candies, to a row of n = num_people people in the following way:
 
     We then give 1 candy to the first person, 2 candies to the second person, and so on until we give n candies to the last person.
 
-    Then, we go back to the start of the row, giving n + 1 candies to the first person, n + 2 candies to the second person, and so on until we give 2 * n candies to the last person.
+    Then, we go back to the start of the row, giving n + 1 candies to the first person, n + 2 candies to the second person, 
+    and so on until we give 2 * n candies to the last person.
 
-    This process repeats (with us giving one more candy each time, and moving to the start of the row after we reach the end) until we run out of candies.  The last person will receive all of our remaining candies (not necessarily one more than the previous gift).
+    This process repeats (with us giving one more candy each time, and moving to the start of the row after we reach the end)
+    until we run out of candies.  The last person will receive all of our remaining candies (not necessarily one more than the previous gift).
 
     Return an array (of length num_people and sum candies) that represents the final distribution of candies.
 
@@ -38,11 +42,37 @@
  */
 
  /*
-  * APPROACH: 
+  * APPROACH: create result array filled with 0s with length num_people. loop while there are still candies
+  * left, keep adding one to each array position until num_people - 1. Once there, add num_people + 1 toscandy
+  * do this if the current position is divisible by num_people - 1. Return the result array.
   */
 
 
 
 public class distributeCandiesToPeople_1103 {
-    
+    public static int[] distributeCandies(int candies, int num_people) {
+        int n = num_people;
+        int[] res = new int[n];
+        int currCandy = 1;
+        int index = 0;
+
+        while (candies > 0) {
+            res[index] += currCandy;
+            candies -= currCandy;
+            index++;
+            currCandy++;
+
+            if (index == n) {
+                index = 0;
+            }
+            if (currCandy > candies) {
+                currCandy = candies;
+            }
+            
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(distributeCandies(10, 3)));
+    }
 }
