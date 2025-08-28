506f0ad9bd52ffa4a2a40575b20d876df2168fd5
ChristineGoGo <brandywang22@gmail.com>
2025-08-27 21:40:42 -0600

    files & folders organization

--- linked_lists/101_symmetric_tree.py
+++ linked_lists/101_symmetric_tree.py
@@ -1,55 +1,101 @@
 """
-    Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+    Given the root of a binary tree, check whether it is a
+    mirror of itself (i.e., symmetric around its center).
 """
-
-# Definition for a binary tree node.
-
-class TreeNode(object):
+class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
-        self.right = right
-        
-        
-class Solution(object):
-    def isSymmetric(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        return self.isMirror(root.left, root.right)
-    
-    def isMirror(self, a, b):
-        if a == None and b == None:
+        self.right= right
+
+# # APPROACH 1 USING STACK
+# def isSymmetric(root):
+#     stack = [[root.left, root.right]]
+
+#     while len(stack) > 0:
+#         current = stack.pop()
+#         if current[0] == None and current[1] == None:
+#             continue
+
+#         if (current[0] == None or current[1] == None ) or (current[0].val != current[1].val):
+#             return False
+
+
+#         stack.append([current[0].left, current[1].right])
+#         stack.append([current[0].right, current[1].left])
+
+#     return True
+
+# APPROACH 2 USING RECURSIVELY
+def isSymmetric(root):
+    def checkMirror(l, r):
+        if l == None and r == None:
             return True
-        if a != None and b!= None:
-            return (a.val == b.val) and self.isMirror(a.left, b.right) and self.isMirror(a.right, b.left)
-        
-   
+
+        if l == None or r == None:
+            return False
+
+        return (l.val == r.val) and checkMirror(l.left, r.right) and checkMirror(l.right, r.left)
 
 
-if __name__ == '__main__':
-    a = TreeNode(1)
-    b = TreeNode(2)
-    c = TreeNode(2)
-    d = TreeNode(3)
-    e = TreeNode(4)
-    f = TreeNode(4)
-    g = TreeNode(3)
+    return checkMirror(root, root)
 
-    ##           1
-    ##        /     \
-    ##       2       2
-    ##      / \     / \
-    ##    3   4     4   3
-    ## Output: True
-    
-    a.left = b
-    a.right = c 
-    b.left = d
-    b.right = e
-    c.left = f
-    c.right = g
-    
-    s = Solution()
-    print(s.isSymmetric(a))
+
+
+
+
+# tests
+#        1
+#     /    \
+#    2      2
+#   /  \   /  \
+#  3    4 4    3
+
+a = TreeNode(1)
+b = TreeNode(2)
+c = TreeNode(2)
+d = TreeNode(3)
+e = TreeNode(4)
+f = TreeNode(4)
+g = TreeNode(3)
+
+a.right = b
+a.left = c
+b.right = d
+b.left = e
+c.right = f
+c.left = g
+print(f'isSymmetric(a): {isSymmetric(a)} ') #True
+
+
+# #        1
+# #     /    \
+# #    2      2
+# #   /  \   /  \
+# #       3      3
+
+# a = TreeNode(1)
+# b = TreeNode(2)
+# c = TreeNode(2)
+# d = TreeNode(3)
+# e = TreeNode(3)
+
+# a.right = b
+# a.left = c
+# b.right = d
+# c.right = e
+
+#        1
+#     /    \
+#    2      3
+
+
+# a = TreeNode(1)
+# b = TreeNode(2)
+# c = TreeNode(3)
+
+# a.right = b
+# a.left = c
+
+
+
