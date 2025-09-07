b595fab38d4e9bcdb7c93de13daf4cd42207942d
ChristineGoGo <brandywang22@gmail.com>
2025-09-07 14:05:53 -0400

    leetcode 2525

---
+++ math/mathcpp/categorize_box_according_to_criteria_2525.cpp
@@ -0,0 +1,88 @@
+/*
+    Given four integers length, width, height, and
+    mass, representing the dimensions and mass of a box,
+    respectively, return a string representing the category of the box.
+
+    The box is "Bulky" if:
+    Any of the dimensions of the box is greater or equal to 104.
+    Or, the volume of the box is greater or equal to 109.
+    If the mass of the box is greater or equal to 100, it is "Heavy".
+    If the box is both "Bulky" and "Heavy", then its category is "Both".
+    If the box is neither "Bulky" nor "Heavy", then its category is "Neither".
+    If the box is "Bulky" but not "Heavy", then its category is "Bulky".
+    If the box is "Heavy" but not "Bulky", then its category is "Heavy".
+    Note that the volume of the box is the product of its length, width and height.
+
+    
+
+    Example 1:
+
+    Input: length = 1000, width = 35, height = 700, mass = 300
+    Output: "Heavy"
+    Explanation: 
+    None of the dimensions of the box is greater or equal to 104. 
+    Its volume = 24500000 <= 109. So it cannot be categorized as "Bulky".
+    However mass >= 100, so the box is "Heavy".
+    Since the box is not "Bulky" but "Heavy", we return "Heavy".
+    Example 2:
+
+    Input: length = 200, width = 50, height = 800, mass = 50
+    Output: "Neither"
+    Explanation: 
+    None of the dimensions of the box is greater or equal to 104.
+    Its volume = 8 * 106 <= 109. So it cannot be categorized as "Bulky".
+    Its mass is also less than 100, so it cannot be categorized as "Heavy" either. 
+    Since its neither of the two above categories, we return "Neither".
+    
+
+    Constraints:
+
+    1 <= length, width, height <= 105
+    1 <= mass <= 103
+
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// APPROACH: Use a vector to track if Bulky or Heavy
+string categorizeBox(int length, int width, int height, int mass) {
+    int bulky = 10000;
+    long long volume = 1LL * length * width * height;
+    vector<string> categories;
+    int total = 0;
+
+    if (length >= bulky || width >= bulky || height>= bulky || volume >= 1000000000 ) {
+        categories.push_back("Bulky") ;
+    } 
+    
+    if (mass >= 100 ) {
+        categories.push_back("Heavy");
+    }
+
+    int b = 0;
+    int h = 0;
+    for (string item: categories) {
+        if (item == "Heavy") {
+            total++;
+            h++;
+        } else if (item == "Bulky") {
+            total++;
+            b++;
+        }
+    }
+
+    if (total == 2) {
+        return "Both";
+    } else if (b == 0 && h == 0) {
+        return "Neither";
+    } else if (b > 0 && h == 0) {
+        return "Bulky";
+    } else {
+        return "Heavy";
+    }
+
+    return "";
+}
+
