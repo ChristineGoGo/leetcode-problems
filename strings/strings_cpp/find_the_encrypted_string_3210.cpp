8701ad3788b58521e6f419c468cf3d4145684667
ChristineGoGo <brandywang22@gmail.com>
2025-11-10 06:24:15 -0500

    leetcode 3210

---
+++ strings/strings_cpp/find_the_encrypted_string_3210.cpp
@@ -0,0 +1,61 @@
+/*
+
+    You are given a string s and an integer k. Encrypt the string using the following algorithm:
+
+    For each character c in s, replace c with the kth character after c in the string (in a cyclic manner).
+    Return the encrypted string.
+
+    
+
+    Example 1:
+
+    Input: s = "dart", k = 3
+
+    Output: "tdar"
+
+    Explanation:
+
+    For i = 0, the 3rd character after 'd' is 't'.
+    For i = 1, the 3rd character after 'a' is 'd'.
+    For i = 2, the 3rd character after 'r' is 'a'.
+    For i = 3, the 3rd character after 't' is 'r'.
+    Example 2:
+
+    Input: s = "aaa", k = 1
+
+    Output: "aaa"
+
+    Explanation:
+
+    As all the characters are the same, the encrypted string will also be the same.
+
+    
+
+    Constraints:
+
+    1 <= s.length <= 100
+    1 <= k <= 104
+    s consists only of lowercase English letters.
+*/
+
+#include <iostream>
+using namespace std;
+
+   // APPROACH: Loop for each character in s, after creating 
+    // an empty result string, with current index being i and 
+    // n being the length of s, add s[i + k % n] to the 
+    // result string
+
+string getEncryptedString(string s, int k) {
+    int n = s.length();
+    string res = "";
+
+    for( int i = 0; i < n; i++) {
+        int toReplace = (i + k) % n;
+        res += s[toReplace];
+
+    }
+
+    return res;
+    
+}
