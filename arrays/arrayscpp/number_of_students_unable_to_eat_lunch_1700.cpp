226569857c4d47acec454be034060d0acee871ed
ChristineGoGo <brandywang22@gmail.com>
2025-12-08 08:44:16 -0500

    leetcode 1700 without spaces

--- arrays/arrayscpp/number_of_students_unable_to_eat_lunch_1700.cpp
+++ arrays/arrayscpp/number_of_students_unable_to_eat_lunch_1700.cpp
@@ -1,29 +1,29 @@
 /*
 
-    The school cafeteria offers circular and square sandwiches at lunch break, 
-    referred to by numbers 0 and 1 respectively. All students stand in a queue. 
+    The school cafeteria offers circular and square sandwiches at lunch break,
+    referred to by numbers 0 and 1 respectively. All students stand in a queue.
     Each student either prefers square or circular sandwiches.
 
-    The number of sandwiches in the cafeteria is equal to the number of students. 
+    The number of sandwiches in the cafeteria is equal to the number of students.
     The sandwiches are placed in a stack. At each step:
 
     If the student at the front of the queue prefers the sandwich on the top of the
     stack, they will take it and leave the queue.
     Otherwise, they will leave it and go to the queue's end.
-    This continues until none of the queue students want to take the top sandwich 
+    This continues until none of the queue students want to take the top sandwich
     and are thus unable to eat.
 
-    You are given two integer arrays students and sandwiches where sandwiches[i] 
-    is the type of the i​​​​​​th sandwich in the stack (i = 0 is the top of the stack) 
-    and students[j] is the preference of the j​​​​​​th student in the initial queue (j = 0 is 
+    You are given two integer arrays students and sandwiches where sandwiches[i]
+    is the type of the i​​​​​​th sandwich in the stack (i = 0 is the top of the stack)
+    and students[j] is the preference of the j​​​​​​th student in the initial queue (j = 0 is
     the front of the queue). Return the number of students that are unable to eat.
 
-    
+
 
     Example 1:
 
     Input: students = [1,1,0,0], sandwiches = [0,1,0,1]
-    Output: 0 
+    Output: 0
     Explanation:
     - Front student leaves the top sandwich and returns to the end of the line making students = [1,0,0,1].
     - Front student leaves the top sandwich and returns to the end of the line making students = [0,0,1,1].
@@ -38,7 +38,7 @@
 
     Input: students = [1,1,1,0,0,1], sandwiches = [1,0,0,0,1,1]
     Output: 3
-    
+
 
     Constraints:
 
@@ -62,7 +62,7 @@ using namespace std;
     // APPROACH: Use two queues for the students and the sandwiches
     // initialize a variable to track the students refusing the top sandwiches
     // loop while there are students and sandwiches in the queues as well as students willing to accept top sandwich:
-    //      - if the sandwich is what the student wants remove the student from the queue and reset the 
+    //      - if the sandwich is what the student wants remove the student from the queue and reset the
     //        students refusing to 0 and remove the sandwich from the queue as well.
     //      - else send the student to the back of the line, add to the students refusing variable
     // return the students refusing variable
@@ -103,7 +103,7 @@ int countStudents(vector<int>& students, vector<int>& sandwiches) {
     // return result
     return studentRefused;
 
-    
+
 }
 
 
