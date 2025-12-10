d36a3591a8d0ca9cbe6658f6c8f785d9384da1e9
ChristineGoGo <brandywang22@gmail.com>
2025-12-10 12:43:01 -0500

    leetcode 3146

---
+++ strings/strings_cpp/permutation_difference_between_two_strings_3146.cpp
@@ -0,0 +1,68 @@
+/*
+    You are given two strings s and t such that every character
+    occurs at most once in s and t is a permutation of s.
+
+    The permutation difference between s and t is defined as the
+    sum of the absolute difference between the index of the occurrence
+    of each character in s and the index of the occurrence of the same character in t.
+
+    Return the permutation difference between s and t.
+
+    Example 1:
+
+    Input: s = "abc", t = "bac"
+
+    Output: 2
+
+    Explanation:
+
+    For s = "abc" and t = "bac", the permutation difference of s and t is equal to the sum of:
+
+    The absolute difference between the index of the occurrence of "a" in s and the index of the occurrence of "a" in t.
+    The absolute difference between the index of the occurrence of "b" in s and the index of the occurrence of "b" in t.
+    The absolute difference between the index of the occurrence of "c" in s and the index of the occurrence of "c" in t.
+    That is, the permutation difference between s and t is equal to |0 - 1| + |1 - 0| + |2 - 2| = 2.
+
+    Example 2:
+
+    Input: s = "abcde", t = "edbac"
+
+    Output: 12
+
+    Explanation: The permutation difference between s and t is equal to |0 - 3| + |1 - 2| + |2 - 4| + |3 - 1| + |4 - 0| = 12.
+
+
+
+    Constraints:
+
+    1 <= s.length <= 26
+    Each character occurs at most once in s.
+    t is a permutation of s.
+    s consists only of lowercase English letters.
+
+*/
+
+// APPROACH: Create a map t to store it's indices
+// and characters, variable to return the permuatation difference
+// loop through s to find the absolute difference and
+// add to the permutation difference return the permutation difference
+
+int findPermutationDifference(string s, string t) {
+    map<char, int> tMap;
+
+    int n = s.length(), m =  t.length(), pd = 0;
+
+    for (int i = 0; i < m; i++) {
+        tMap[t[i]] = i;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int ad = abs(i - tMap[s[i]]);
+        pd += ad;
+    }
+
+    return pd;
+}
+
+
+
