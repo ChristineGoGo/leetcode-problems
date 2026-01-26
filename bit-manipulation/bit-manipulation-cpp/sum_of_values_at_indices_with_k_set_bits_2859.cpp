ddc7e8c7faa3fd07cee78fecd38af6f9e245196f
ChristineGoGo <brandywang22@gmail.com>
2026-01-26 05:00:32 -0500

    leetcode 2859

---
+++ bit-manipulation/bit-manipulation-cpp/sum_of_values_at_indices_with_k_set_bits_2859.cpp
@@ -0,0 +1,73 @@
+/*
+
+    You are given a 0-indexed integer array nums and an integer k.
+
+    Return an integer that denotes the sum of elements in nums whose
+    corresponding indices have exactly k set bits in their binary representation.
+
+    The set bits in an integer are the 1's present when it is written in binary.
+
+    For example, the binary representation of 21 is 10101, which has 3 set bits.
+
+
+    Example 1:
+
+    Input: nums = [5,10,1,5,2], k = 1
+    Output: 13
+    Explanation: The binary representation of the indices are:
+    0 = 0002
+    1 = 0012
+    2 = 0102
+    3 = 0112
+    4 = 1002
+    Indices 1, 2, and 4 have k = 1 set bits in their binary representation.
+    Hence, the answer is nums[1] + nums[2] + nums[4] = 13.
+
+    Example 2:
+    Input: nums = [4,3,2,1], k = 2
+    Output: 1
+    Explanation: The binary representation of the indices are:
+    0 = 002
+    1 = 012
+    2 = 102
+    3 = 112
+    Only index 3 has k = 2 set bits in its binary representation.
+    Hence, the answer is nums[3] = 1.
+
+
+    Constraints:
+
+    1 <= nums.length <= 1000
+    1 <= nums[i] <= 10**5
+    0 <= k <= 10
+*/
+
+// APPROACH: Create a variable to store the resulting sum
+// Create a map to store indices upto 1000 and their binary representations
+// Find the corresponding binary representations
+// loop through each index in nums adding the sum of binary rep equal to k
+// return the sum
+
+int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+    int res = 0; int n = nums.size();
+    map<int, int> binRep;
+
+    for (int i = 0; i < 1000; i++) {
+        int curr = i; int reps = 0;
+
+        while(curr > 0) {
+            int b = curr % 2;
+            if (b == 1) reps++;
+            curr /= 2;
+        }
+
+        binRep[i] = reps;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (binRep[i] == k) res += nums[i];
+    }
+
+    return res;
+}
+
