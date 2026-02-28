b202874b02113f40e749716f11d28dc126338a60
ChristineGoGo <brandywang22@gmail.com>
2026-02-28 18:58:26 -0500

    leetcode 748

---
+++ strings/strings_cpp/shortest_completing_word_748.cpp
@@ -0,0 +1,97 @@
+/*
+    Given a string licensePlate and an array of strings words,
+    find the shortest completing word in words.
+
+    A completing word is a word that contains all the letters
+    in licensePlate. Ignore numbers and spaces in licensePlate,
+    and treat letters as case insensitive. If a letter appears more than
+    once in licensePlate, then it must appear in the word the same number of times or more.
+
+    For example, if licensePlate = "aBc 12c", then it contains letters 'a', 'b'
+    (ignoring case), and 'c' twice. Possible completing words are "abccdef", "caaacab", and "cbca".
+
+    Return the shortest completing word in words. It is guaranteed an answer exists.
+    If there are multiple shortest completing words, return the first one that occurs in words.
+
+
+
+    Example 1:
+
+    Input: licensePlate = "1s3 PSt", words = ["step","steps","stripe","stepple"]
+    Output: "steps"
+    Explanation: licensePlate contains letters 's', 'p', 's' (ignoring case), and 't'.
+    "step" contains 't' and 'p', but only contains 1 's'.
+    "steps" contains 't', 'p', and both 's' characters.
+    "stripe" is missing an 's'.
+    "stepple" is missing an 's'.
+    Since "steps" is the only word containing all the letters, that is the answer.
+
+    Example 2:
+    Input: licensePlate = "1s3 456", words = ["looks","pest","stew","show"]
+    Output: "pest"
+    Explanation: licensePlate only contains the letter 's'. All the words contain 's',
+    but among these "pest", "stew", and "show" are shortest. The answer is "pest"
+    because it is the word that appears earliest of the 3.
+
+
+    Constraints:
+
+    1 <= licensePlate.length <= 7
+    licensePlate contains digits, letters (uppercase or lowercase), or space ' '.
+    1 <= words.length <= 1000
+    1 <= words[i].length <= 15
+    words[i] consists of lower case English letters.
+
+*/
+// APPROACH: Create a map to store all the letters in license plate
+// and therir count convert the letters to lowercase
+// Loop through words and add each letter into a map with their frequency
+// Loop through each letter in each word and return when their is one 
+// with all the letters found in the licence plate
+string shortestCompletingWord(string licensePlate, vector<string>& words) {
+    
+        map<char, int> lpFreq;
+    
+
+        for (char letter: licensePlate){
+            char l = tolower(letter);
+            int i = l;
+            if ( l > 96 && l < 124) {
+                lpFreq[l]++;
+            }
+            
+        }
+
+        int s = 15; string res;
+        for (string word: words) {
+            int found = 1;
+            map<char, int> cwFreq;
+            for(char letter: word) {
+                cwFreq[letter]++;
+            }
+
+            for (auto vals: lpFreq) {
+                cout << vals.first << "\n";
+                cout << vals.second << "\n";
+
+                if (cwFreq[vals.first] < vals.second ) found = 0;
+            }
+
+            if (found == 1 && word.size() <s) {
+                res = word;
+                s = word.size();
+            }
+
+        }
+
+        return res;
+    
+}
+
+
+
+
+
+
+
+
