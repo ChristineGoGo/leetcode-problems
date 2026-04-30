b7e673354f620d4a7d4421cc35d4e9adeeacf012
ChristineGoGo <brandywang22@gmail.com>
2026-04-30 11:26:13 -0400

    leetcode 1539

---
+++ arrays/arrayscpp/kth_missing_positive_number_1539.cpp
@@ -0,0 +1,59 @@
+/*
+    Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+
+    Return the kth positive integer that is missing from this array.
+
+
+    Example 1:
+
+    Input: arr = [2,3,4,7,11], k = 5
+    Output: 9
+    Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+
+    Example 2:
+
+    Input: arr = [1,2,3,4], k = 2
+    Output: 6
+    Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+
+
+    Constraints:
+
+        - 1 <= arr.length <= 1000
+        - 1 <= arr[i] <= 1000
+        - 1 <= k <= 1000
+        - arr[i] < arr[j] for 1 <= i < j <= arr.length
+
+
+    Follow up:
+
+    Could you solve this problem in less than O(n) complexity?
+*/
+
+// APPROACH: Brute force Create a hashmap to store all the values in arr
+// each value has a true flag
+// Loop and collect all the missing integers from 0 to 1000
+// and store them in a vector called missing.
+// return missing[k - 1]
+int findKthPositive(vector<int>& arr, int k) {
+    map<int, bool> arrTally;
+    vector<int> missing;
+
+    for (int num: arr) {
+        arrTally[num] = true;
+    }
+
+    for (int i = 1; i <= 10000; i++) {
+        if (!arrTally[i]) missing.push_back(i);
+    }
+    
+    return missing[k - 1];
+    
+}
+
+
+
+
+
+
+
